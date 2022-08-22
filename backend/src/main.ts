2daa718f1fd6e65f98adcdbf65e06a7f40ec002e
maria-d-ferreira <maria.d.ferreira@sapo.pt>
2022-08-22 15:07:35 +0100

    esModuleInterop-true

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-import cookieParser = require('cookie-parser');
+
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
