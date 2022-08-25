d5b0010d4a230862400030daa05e0b6e889c00f6
maria-d-ferreira <maria.d.ferreira@sapo.pt>
2022-08-25 15:51:44 +0100

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,19 +7,7 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const list = [
-    'http://localhost:5000',
-    'https://school-frontend-six.vercel.app',
-  ];
-  app.enableCors({
-    origin: function (origin, callback) {
-      if (!origin || list.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-  });
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
