4493f0a5a2d43578610b71c710e657dc4080082c
maria-d-ferreira <maria.d.ferreira@sapo.pt>
2022-08-25 17:01:40 +0100

    change-url

--- frontend/src/apis/users.api.ts
+++ frontend/src/apis/users.api.ts
@@ -9,12 +9,13 @@ import { User } from "../schemas/User";
 export const usersApi = createApi({
   reducerPath: "usersApi",
 
-  baseQuery: fetchBaseQuery({
-    baseUrl: "https://school-backend-e1jhkszow-maria-ferreira.vercel.app/users",
-  }),
   // baseQuery: fetchBaseQuery({
-  //   baseUrl: process.env.REACT_APP_BASE_URL + "/users",
+  //   baseUrl: "https://school-backend-e1jhkszow-maria-ferreira.vercel.app/users",
   // }),
+
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.REACT_APP_BASE_URL + "/users",
+  }),
 
   endpoints: build => ({
     createUser: build.mutation<User, CreateUserRequest>({
